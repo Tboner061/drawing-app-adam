bd49354cf6af1bb0dac3c655121ab8aa653d00bd
dr-montasir <montasir1400@gmail.com>
2024-05-22 21:14:09 +0300

    Lesson 8
    JavaAcript (Year Function)

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-const add = (x, y) => {
-  return x + y;
-};
+// const add = (x, y) => {
+//   return x + y;
+// };
+
+// const startTime = () => {
+//   var today = new Date();
+//   var h = today.getHours();
+//   var m = today.getMinutes();
+//   var s = today.getSeconds();
+
+//   return [h, m, s].join(':');
+// };
+
+// window.alert(startTime());
+// window.alert(startTime());
+// window.alert(startTime());
