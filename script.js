20d14673ba33cb17fa103c5dd88cb70840c4c655
dr-montasir <montasir1400@gmail.com>
2024-05-30 21:50:39 +0300

    Lesson 11
    Splash screen part 2.
    
    fadeIn + js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-// const add = (x, y) => {
-//   return x + y;
-// };
 
-// const startTime = () => {
-//   var today = new Date();
-//   var h = today.getHours();
-//   var m = today.getMinutes();
-//   var s = today.getSeconds();
-
-//   return [h, m, s].join(':');
-// };
-
-// window.alert(startTime());
-// window.alert(startTime());
-// window.alert(startTime());
