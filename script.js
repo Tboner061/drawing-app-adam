745fec575866026297137037cac5e2ee1bf4f791
dr-montasir <montasir1400@gmail.com>
2024-06-12 21:39:21 +0300

    Lesson 14
    Canvas part 1

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+///////////////////////////////////
 
+var c = document.getElementById('myCanvas');
+var ctx = c.getContext('2d');
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 100);
+ctx.stroke();
+
+// var c = document.getElementById('myCanvas');
+// var ctx = c.getContext('2d');
+ctx.beginPath();
+ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+// var c = document.getElementById('myCanvas');
+// var ctx = c.getContext('2d');
+// ctx.font = '30px Arial';
+// ctx.fillText('Hello World', 10, 50);
+
+// var c = document.getElementById('myCanvas');
+// var ctx = c.getContext('2d');
+ctx.font = '30px Arial';
+ctx.fillText(new Date().getFullYear() - 1, 62, 62);
