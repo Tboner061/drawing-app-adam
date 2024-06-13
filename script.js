0c68a0d995f775e263b398c3724b782dfc124de6
dr-montasir <montasir1400@gmail.com>
2024-06-13 21:58:13 +0300

    Lesson 15
    Tools Board: List tag

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-///////////////////////////////////
+/**
+ * @name splash
+ * @description splash screen function
+ */
 
-var c = document.getElementById('myCanvas');
-var ctx = c.getContext('2d');
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 100);
-ctx.stroke();
+const splash = document.querySelector('.splash');
+
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
+    setTimeout(() => {
+      splash.classList.add('display-none');
+    });
+  },
+  2000
+);
+
+// The end of the splash screen code
 
 // var c = document.getElementById('myCanvas');
 // var ctx = c.getContext('2d');
-ctx.beginPath();
-ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-ctx.stroke();
+// ctx.moveTo(0, 0);
+// ctx.lineTo(200, 100);
+// ctx.stroke();
 
-// var c = document.getElementById('myCanvas');
-// var ctx = c.getContext('2d');
+// // var c = document.getElementById('myCanvas');
+// // var ctx = c.getContext('2d');
+// ctx.beginPath();
+// ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+// ctx.stroke();
+
+// // var c = document.getElementById('myCanvas');
+// // var ctx = c.getContext('2d');
+// // ctx.font = '30px Arial';
+// // ctx.fillText('Hello World', 10, 50);
+
+// // var c = document.getElementById('myCanvas');
+// // var ctx = c.getContext('2d');
 // ctx.font = '30px Arial';
-// ctx.fillText('Hello World', 10, 50);
-
-// var c = document.getElementById('myCanvas');
-// var ctx = c.getContext('2d');
-ctx.font = '30px Arial';
-ctx.fillText(new Date().getFullYear() - 1, 62, 62);
+// ctx.fillText(new Date().getFullYear() - 1, 62, 62);
